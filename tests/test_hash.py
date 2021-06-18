2ee73c469373f1190ff48732505898e303674576
posterrieri <adamdorucu@gmail.com>
2021-06-18 23:06:54 +0300

    sha512

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -1,5 +1,5 @@
 import hashlib
-from hdwallet.sha256 import sha256
+from hdwallet.hash import sha256, sha512
 
 test_arr = [
   b'',
@@ -8,8 +8,15 @@ test_arr = [
   b'do you hear the people sing, singing the song of angry man it is the music of the people whowill now be slaves again' * 10
 ]
 
+
 def test_sha256():
   for x in test_arr:
     hl = hashlib.sha256(x).hexdigest()
-    my = sha256(x)
+    my = sha256(x).hex()
+    assert hl == my
+
+def test_sha512():
+  for x in test_arr:
+    hl = hashlib.sha512(x).hexdigest()
+    my = sha512(x).hex()
     assert hl == my
